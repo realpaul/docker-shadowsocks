dd768ca9995936e2116f4d1d5c5bfae38dd88af8
Paul Lv <paul@iamlv.com>
2017-10-26 18:14:09 +0800

    Update wording a bit

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,11 +16,11 @@ else
   KCPTUN_TARGET_PORT=$OBFS_PORT
 fi
 
-echo "Starting Shadowsocks Server on port $SHADOWSOCKS_PORT..."
+echo "Starting Shadowsocks Server on port $SHADOWSOCKS_PORT with crypto $SHADOWSOCKS_CRYPTO..."
 ss-server -s 0.0.0.0 -p "$SHADOWSOCKS_PORT" -k "$SHADOWSOCKS_PASSWORD" -m "$SHADOWSOCKS_CRYPTO" --fast-open &
 
-echo "Starting Obfs Server on port $OBFS_PORT..."
+echo "Starting Obfs Server on port $OBFS_PORT over $SHADOWSOCKS_PORT with protocol $OBFS_PROTOCOL..."
 obfs-server -r "127.0.0.1:$SHADOWSOCKS_PORT" -p "$OBFS_PORT" --obfs "$OBFS_PROTOCOL" &
 
-echo "Starting Kcptun Server on udp port $KCPTUN_PORT over $KCPTUN_TARGET_PORT..."
+echo "Starting Kcptun Server on udp port $KCPTUN_PORT over $KCPTUN_TARGET_PORT with crypto $KCPTUN_CRYPTO..."
 kcptun-server --target "127.0.0.1:$KCPTUN_TARGET_PORT" --listen ":$KCPTUN_PORT" --mode "$KCPTUN_MODE" --key "$KCPTUN_KEY" --crypt "$KCPTUN_CRYPTO" --mtu 1350 --sndwnd 1024 --rcvwnd 1024
